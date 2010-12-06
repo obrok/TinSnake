d456a8dec1a0c734ecaeb1563488f6de53739a51
Pawe   Obrok <pawel.obrok@gmail.com>
2010-12-06 13:08:30 +0100

    Writing images to disk

--- src/pl/edu/agh/tinsnake/PrepareMap.java
+++ src/pl/edu/agh/tinsnake/PrepareMap.java
@@ -1,22 +1,28 @@
 package pl.edu.agh.tinsnake;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class PrepareMap extends Activity implements OnTouchListener,
-		OnClickListener {
+		OnClickListener, android.content.DialogInterface.OnClickListener {
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -29,14 +35,15 @@ public class PrepareMap extends Activity implements OnTouchListener,
 		this.findViewById(R.id.map).setOnTouchListener(this);
 		this.findViewById(R.id.map).setOnClickListener(this);
 		this.findViewById(R.id.zoomOutButton).setOnClickListener(this);
+		this.findViewById(R.id.saveButton).setOnClickListener(this);
 		refreshMap();
 	}
 
 	private EarthCoordinates coordinates;
 	private float lastX, lastY;
+	private Bitmap bitmap;
 
 	private void refreshMap() {
-		Bitmap bitmap = null;
 		HttpURLConnection conn = null;
 
 		try {
@@ -76,9 +83,47 @@ public class PrepareMap extends Activity implements OnTouchListener,
 		if (v.getId() == R.id.map) {
 			coordinates.zoomIn(lastX, lastY);
 			refreshMap();
-		} else if(v.getId() == R.id.zoomOutButton){
+		} else if (v.getId() == R.id.zoomOutButton) {
 			coordinates.zoomOut();
 			refreshMap();
+		} else if (v.getId() == R.id.saveButton) {
+			showInputDialog();
 		}
+	}
+
+	private EditText input;
+	private void showInputDialog() {
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+		alert.setTitle("Input map name");
+
+		// Set an EditText view to get user input
+		input = new EditText(this);
+		alert.setView(input);
+		alert.setPositiveButton("Ok", this);
+		alert.show();
+	}
+
+	private void saveMap(String name) {
+		try {
+			File dir = new File(Environment.getExternalStorageDirectory()
+					.getAbsolutePath()
+					+ File.separator + "mapsfolder");
+			dir.mkdirs();
+
+			File file = new File(dir.getPath() + File.separator + name + ".jpg");
+
+			FileOutputStream out = new FileOutputStream(file);
+			bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+		} catch (Exception e) {
+			((TextView) this.findViewById(R.id.prepareDebug)).setText(e
+					.getClass().getCanonicalName()
+					+ " " + e.getMessage());
+		}
+	}
+
+	@Override
+	public void onClick(DialogInterface dialog, int which) {
+		saveMap(input.getText().toString());
 	}
 }
