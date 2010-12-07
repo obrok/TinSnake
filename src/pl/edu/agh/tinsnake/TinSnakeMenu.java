e52b945276e7f29474883b2962b19d7ca84158a0
marcnowa <marcin.k.nowak@gmail.com>
2010-12-07 11:40:33 +0100

    fajny alert zamiast spinnera

--- src/pl/edu/agh/tinsnake/TinSnakeMenu.java
+++ src/pl/edu/agh/tinsnake/TinSnakeMenu.java
@@ -3,17 +3,16 @@ package pl.edu.agh.tinsnake;
 import java.io.File;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.Toast;
 
 public class TinSnakeMenu extends Activity implements OnClickListener {
-	
-	private Spinner mapNameSpinner;
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -22,15 +21,6 @@ public class TinSnakeMenu extends Activity implements OnClickListener {
 		setContentView(R.layout.main);
 		this.findViewById(R.id.prepareButton).setOnClickListener(this);
 		this.findViewById(R.id.showButton).setOnClickListener(this);
-		
-		File dir = new File(Environment.getExternalStorageDirectory()
-				.getAbsolutePath()
-				+ File.separator + "mapsfolder");
-
-		mapNameSpinner = (Spinner) findViewById(R.id.chooseMapSpinner);
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_spinner_item, dir.list());
-		mapNameSpinner.setAdapter(adapter);
 	}
 
 	@Override
@@ -39,8 +29,34 @@ public class TinSnakeMenu extends Activity implements OnClickListener {
 		if (v.getId() == R.id.prepareButton) {
 			intent = new Intent(v.getContext(), PrepareMap.class);
 		} else if (v.getId() == R.id.showButton) {
-			intent = new Intent(v.getContext(), ShowMap.class);
-			intent.putExtra("mapName", mapNameSpinner.getSelectedItem().toString());
+			
+	        File dir = new File(Environment.getExternalStorageDirectory()
+					.getAbsolutePath()
+					+ File.separator + "mapsfolder");
+			
+			final String[] items = dir.list();
+			
+			if (items.length == 0){
+				Toast.makeText(getApplicationContext(), "No maps :(", Toast.LENGTH_SHORT).show();
+				return;
+			}
+
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle("Pick a map");
+			builder.setItems(items, new DialogInterface.OnClickListener() {
+			    public void onClick(DialogInterface dialog, int item) {
+			        Toast.makeText(getApplicationContext(), items[item], Toast.LENGTH_SHORT).show();
+			        String chosen = items[item].toString();
+			        
+			        Intent i = new Intent(getBaseContext(), ShowMap.class);
+			        i.putExtra("mapName", chosen);
+			        startActivity(i);
+			    }
+			});
+			AlertDialog alert = builder.create();
+			alert.show();
+			
+			return;
 		}
 
 		startActivity(intent);
